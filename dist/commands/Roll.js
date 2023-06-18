dfba3ec396473ec62d0cecbe520a0a27e0f104f5
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-06-18 11:06:06 -0300

    chore: altera versão de prod

--- dist/commands/Roll.js
+++ dist/commands/Roll.js
@@ -2,7 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Roll = void 0;
 function Roll(interaction) {
-    const rolada = String(Math.floor(Math.random() * 20));
-    interaction.reply(`Sua rolada deu ${rolada}`);
+    const rolada = String(Math.floor(Math.random() * 100));
+    if (rolada === "0") {
+        interaction.reply(`Sua rolada deu ${rolada}! Você ganhou um grandiozíssimo graveto!`);
+        return;
+    }
+    if (rolada >= "1" && rolada <= "49") {
+        interaction.reply(`Sua rolada deu ${rolada}! Você ganhou uma vara de blaze!`);
+        return;
+    }
+    if (rolada >= "50" && rolada <= "99") {
+        interaction.reply(`Sua rolada deu ${rolada}! Você ganhou uma maçã dourada!`);
+        return;
+    }
+    if (rolada === "100") {
+        interaction.reply(`Sua rolada deu ${rolada}! Você ganhou um peitoral de netherite!`);
+        return;
+    }
 }
 exports.Roll = Roll;
