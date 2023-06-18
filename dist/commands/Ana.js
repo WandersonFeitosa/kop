951a25cfde05180fb9aad3ac671bea93c2b0779c
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-06-18 11:09:35 -0300

    chore: remove pasta de prod

--- dist/commands/Ana.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Ana = void 0;
-function Ana(interaction) {
-    interaction.reply("Eu: 😎 | Vcs: 🤓");
-}
-exports.Ana = Ana;
