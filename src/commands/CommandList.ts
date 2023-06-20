334aa8cca1dce82465248a301b063f365369f158
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-06-19 21:32:06 -0300

    feat: organiza os comandos

---
+++ src/commands/CommandList.ts
@@ -0,0 +1,66 @@
+export const commands = [
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+  {
+    name: "roll",
+    description: "Rola um d20",
+  },
+  {
+    name: "ana",
+    description: "ana",
+  },
+  {
+    name: "plugins",
+    description: "Lista todos os plugins instalados no servidor",
+    options: [
+      {
+        name: "page",
+        description: "Insira a página que deseja visualizar",
+        type: 3,
+      },
+    ],
+  },
+  {
+    name: "pegarid",
+    description: "Pega a id de um jogador",
+    options: [
+      {
+        name: "player",
+        description: "Insira a página que deseja visualizar",
+        required: false,
+        type: 6,
+      },
+    ],
+  },
+  {
+    name: "familia",
+    description: "familia",
+    options: [
+      {
+        name: "sortear",
+        description: "Realiza um sorteio",
+        type: 1,
+      },
+      {
+        name: "vincular",
+        description: "Vincula com uma pessoa do servidor",
+        type: 1,
+        options: [
+          {
+            name: "player",
+            description: "Insira a player que deseja vincular",
+            required: true,
+            type: 6,
+          },
+        ],
+      },
+      {
+        name: "desvincular",
+        description: "Desvincula com uma pessoa do servidor",
+        type: 1,
+      },
+    ],
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -7,62 +7,13 @@ import { Ana } from "./commands/Ana";
 import { Roll } from "./commands/Roll";
 import { Plugins } from "./commands/Plugins";
 import { Sorteio } from "./commands/Sorteio";
+import { commands } from "./commands/CommandList";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const TOKEN = process.env.TOKEN as string;
 const CLIENT_ID = process.env.CLIENT_ID as string;
 const dbUrl = process.env.MONGODB_URI || "";
-
-const commands = [
-  {
-    name: "ping",
-    description: "Replies with Pong!",
-  },
-  {
-    name: "roll",
-    description: "Rola um d20",
-  },
-  {
-    name: "ana",
-    description: "ana",
-  },
-  {
-    name: "plugins",
-    description: "Lista todos os plugins instalados no servidor",
-    options: [
-      {
-        name: "page",
-        description: "Insira a página que deseja visualizar",
-        type: 3,
-      },
-    ],
-  },
-  {
-    name: "sorteio",
-    description: "Realiza um sorteio",
-  },
-  {
-    name: "vincular",
-    description: "Vincula com uma pessoa do servidor",
-  },
-  {
-    name: "desvincular",
-    description: "Remove vinculos",
-  },
-  {
-    name: "pegarid",
-    description: "Pega a id de um jogador",
-    options: [
-      {
-        name: "player",
-        description: "Insira a página que deseja visualizar",
-        required: false,
-        type: 6,
-      },
-    ],
-  },
-];
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
@@ -109,16 +60,18 @@ client.on("interactionCreate", async (interaction: any) => {
     const page = interaction.options.get("page")?.value || 1;
     new Plugins().listPlugins(interaction, page);
   }
-  if (interaction.commandName === "sorteio") {
-    new Sorteio().raffle(interaction);
-  }
-  if (interaction.commandName === "vincular") {
-    new Sorteio().bindUser(interaction);
-  }
-  if (interaction.commandName === "desvincular") {
-    new Sorteio().unbindUser(interaction);
-  }
   if (interaction.commandName === "pegarid") {
     new Sorteio().getID(interaction);
+  }
+  if (interaction.commandName === "familia") {
+    if (interaction.options.getSubcommand() === "sortear") {
+      new Sorteio().raffle(interaction);
+    }
+    if (interaction.options.getSubcommand() === "vincular") {
+      new Sorteio().bindUser(interaction);
+    }
+    if (interaction.options.getSubcommand() === "desvincular") {
+      new Sorteio().unbindUser(interaction);
+    }
   }
 });
