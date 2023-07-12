5de3e37e7533c7c4ffc84c9c1969d32ec624bece
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-07-11 23:03:45 -0300

    feat: adicionar o mensagem adicional para ultima etapa do enigma

--- src/utils/sendPuzzleMsg.ts
+++ src/utils/sendPuzzleMsg.ts
@@ -13,6 +13,16 @@ export function sendPuzzleMsg(message: string) {
     channel.send(message + "\n\n @everyone");
     return;
   }
+  if (message == "ENVIAR IMAGEM") {
+    const announceChannel: any = client.channels.cache.get(
+      "1107698677960687626"
+    );
+    announceChannel.send({
+      content: "|| @everyone https://discord.gg/QfstDap2 ||",
+      files: ["./src/images/descarte.png"],
+    });
+    return;
+  }
   const channel: any = client.channels.cache.get("1119004778589069312");
   if (channel) {
     channel.send(message + "\n\n @everyone");
