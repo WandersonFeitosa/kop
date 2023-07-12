d53587273daee70a54dda7579ea2e9df7e19cac6
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-07-12 01:57:55 -0300

    fix: ajusta texto da função de rolagem

--- src/commands/Roll.ts
+++ src/commands/Roll.ts
@@ -1,28 +1,8 @@
 export function Roll(interaction: any) {
   const rolada = String(Math.floor(Math.random() * 100));
 
-  if (rolada === "0") {
-    interaction.reply(
-      `Sua rolada deu ${rolada}! Você ganhou um grandiozíssimo graveto!`
-    );
-    return;
-  }
-  if (rolada >= "1" && rolada <= "49") {
-    interaction.reply(
-      `Sua rolada deu ${rolada}! Você ganhou uma vara de blaze!`
-    );
-    return;
-  }
-  if (rolada >= "50" && rolada <= "99") {
-    interaction.reply(
-      `Sua rolada deu ${rolada}! Você ganhou uma maçã dourada!`
-    );
-    return;
-  }
-  if (rolada === "100") {
-    interaction.reply(
-      `Sua rolada deu ${rolada}! Você ganhou um peitoral de netherite!`
-    );
-    return;
-  }
+  interaction.reply({
+    content: `Sua rolada deu ${rolada}`,
+  });
+
 }
