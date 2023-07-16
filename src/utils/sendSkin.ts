bbecb5c95908adbcff42298bcfb135fccd0743b7
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-07-16 03:57:49 -0300

    layout: altera a mensagem do comando de skins

--- src/utils/sendSkin.ts
+++ src/utils/sendSkin.ts
@@ -2,7 +2,6 @@ export function sendSkin(interaction: any) {
   //Envia um arquivo e uma mensagem
   interaction.reply({
     content:
-      "Adicione o arquivo na sua pasta de mods e lembrando que ele pode não funcionar 100%, e é recomendável que use o launcher do Forge",
-    files: [`./src/uploads/tl_skin_cape_forge_1.19.3_1.19.4-1.30.jar`],
+      "Para utilizar o sistema de skins do servidor caso você use o Tlauncher siga os passos do vídeo \n\n  https://www.youtube.com/watch?v=42gQPj8Dycw"
   });
 }
