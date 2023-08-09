c397c1aea16f435d9096ea504b19b3346a2d3899
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-08-09 13:27:09 -0300

    feat: adiciona rota para verificar se o usuario é player ou aquiles

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import cors from "cors";
 import { sendPuzzleMsg } from "../utils/sendPuzzleMsg";
 import { sendServerMessage } from "../utils/sendServerStatus";
+import { CheckAllowedUser } from "../utils/checkAllowedUser";
 
 const routes = Router();
 
@@ -12,10 +13,16 @@ routes.post("/puzzle", (req, res) => {
   sendPuzzleMsg(message);
   res.send("ok");
 });
+
 routes.post("/sendMessage", (req, res) => {
   const { channelId, message } = req.body;
   sendServerMessage(channelId, message);
   res.send("ok");
 });
+
+routes.get(
+  "/checkAllowedUser/:username",
+  new CheckAllowedUser().checkAllowedUser
+);
 
 export default routes;
