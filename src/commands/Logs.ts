15c62fcf7928bfc8771ed23b7133aad92961f782
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-09-14 10:37:50 -0300

    feat: finaliza comando de enviar logs do servidor

--- src/commands/Logs.ts
+++ src/commands/Logs.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
 const serverManagerUrl = process.env.SERVER_MANAGER_URL as string;
 const serverManagerToken = process.env.SERVER_MANAGER_TOKEN as string;
+import * as fs from 'fs';
+import { userHasRole } from "../utils/userHasRole";
+import { suport_role_id } from "../utils/rolesId";
+
 
 export class Logs {
     async getLog(interaction: any) {
+        const allowedUser = userHasRole({ userId: interaction.user.id, roleId: suport_role_id });
+
+        if (!allowedUser) {
+            return interaction.reply("Você não tem permissão para executar esse comando");
+        }
         const requestedFile = interaction.options.getString("nome");
         var config = {
             method: "GET",
@@ -28,7 +37,26 @@ export class Logs {
             return interaction.reply(error.response.data.message);
         }
 
-        console.log(response)
+        const file = response.data
+
+        const fileName = requestedFile;
+        await fs.promises.writeFile(`./src/uploads/${fileName}`, file);
+
+        await interaction.reply({
+            files: [{
+                attachment: `./src/uploads/${fileName}`,
+                name: fileName
+            }]
+        });
+
+
+        fs.unlink(`./src/uploads/${fileName}`, (err) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+        })
+
     }
     async getLogsNames(interaction: any) {
         var config = {
