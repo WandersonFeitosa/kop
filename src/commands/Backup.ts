c46561671af45fe9a4e91d17374762954c80bfd1
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-11-08 08:09:25 -0300

    feat: add try catch to the backup command

--- src/commands/Backup.ts
+++ src/commands/Backup.ts
@@ -6,38 +6,45 @@ const serverManagerUrl = process.env.SERVER_MANAGER_URL as string;
 const serverManagerToken = process.env.SERVER_MANAGER_TOKEN as string;
 
 export class Backup {
-    async startBackup(interaction: any) {
+  async startBackup(interaction: any) {
+    const allowedUser = userHasRole({
+      userId: interaction.user.id,
+      roleId: suport_role_id,
+    });
 
-        const allowedUser = userHasRole({ userId: interaction.user.id, roleId: suport_role_id });
-
-        if (!allowedUser) {
-            return interaction.reply("Você não tem permissão para executar esse comando");
-        }
-
-        var config = {
-            method: "GET",
-            url: serverManagerUrl + "/startBackup",
-            headers: {
-                authorization: "Basic " + serverManagerToken,
-            }
-        };
-
-        async function requestBackupStart() {
-            try {
-                const response = await axios(config);
-                return { data: response.data, sucess: true };
-            } catch (err) {
-                return { err, sucess: false };
-            }
-        }
-
-        const response = await requestBackupStart();
-
-        if (response.err) {
-            const error: any = response.err;
-            return interaction.reply(error.response.data.message);
-        }
-
-        return interaction.reply(response.data.message);
+    if (!allowedUser) {
+      return interaction.reply(
+        "Você não tem permissão para executar esse comando"
+      );
     }
+
+    var config = {
+      method: "GET",
+      url: serverManagerUrl + "/startBackup",
+      headers: {
+        authorization: "Basic " + serverManagerToken,
+      },
+    };
+
+    async function requestBackupStart() {
+      try {
+        const response = await axios(config);
+        return { data: response.data, sucess: true };
+      } catch (err) {
+        return { err, sucess: false };
+      }
+    }
+
+    const response = await requestBackupStart();
+    try {
+      if (response.err) {
+        const error: any = response.err;
+        return interaction.reply(error.response.data.message);
+      }
+
+      return interaction.reply(response.data.message);
+    } catch (err) {
+      return interaction.reply("Erro ao executar o comando");
+    }
+  }
 }
